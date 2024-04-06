2338312e798c3f242fba5246dbc19759de5628c0
= <=>
2024-04-06 17:25:42 -0500

    fcommit

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+import { defineConfig } from 'astro/config';
+
+export default defineConfig({
+  site: 'https://picoscience.github.io',
+  base: '/',
+  outDir: 'public',
+  publicDir: 'static',
+});
