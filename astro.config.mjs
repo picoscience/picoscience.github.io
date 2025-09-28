39fdc4aa15bd68ad4ebcd87f2a045eb4fd4619c8
= <=>
2025-09-28 14:16:30 -0500

    feature: fix cv

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
 
 export default defineConfig({
-  site: 'https://picoscience.github.io',  
+  site: 'https://picoscience.github.io',
+  integrations: [tailwind()],
 })
